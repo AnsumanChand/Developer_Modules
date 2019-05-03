bd2854d765f6f70a7ebcb530b9b948505f121225
titan <ansuman72207@gmail.com>
2019-05-03 11:18:41 +0530

    Folder Creator

---
+++ utilities.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+
+
+
+def make_folder(path):
+    os.makedirs(path)
+
+class FolderMaker():
+    
+    def __init__(self,root_location,folder_name):
+        self.folder_name =  folder_name
+        self.folder_location = root_location
+        self.sub_folder = ["Data","Codes","Models","Results"]
+    
+        
+    def create_folder(self):
+        self.full_path = os.path.join(self.folder_location,self.folder_name)
+        if os.path.exists(self.full_path):
+            print(" {} folder already exists".format(self.folder_name))
+        else:
+            make_folder(self.full_path)
+    
+    def create_project(self):
+        
+        for subf in self.sub_folder:
+            full_project_path = os.path.join(self.folder_location,self.folder_name,subf)
+            if os.path.exists(full_project_path):
+                print(" {} {} folder already exists".format(self.folder_name,subf))
+            else:
+                make_folder(full_project_path)
+
+
+                
